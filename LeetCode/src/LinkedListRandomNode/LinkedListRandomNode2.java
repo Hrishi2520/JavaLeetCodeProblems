b6709c51669830c2a03f550c23279cd2f0e539b2
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-03-10 22:30:25 +0530

    LinkedListRandomNode2

---
+++ LeetCode/src/LinkedListRandomNode/LinkedListRandomNode2.java
@@ -0,0 +1,14 @@
+class LinkedListRandomNode2 {
+    ArrayList<Integer> nodes = new ArrayList<Integer>();
+    public Solution(ListNode head) {
+        while (head != null) {
+            nodes.add(head.val);
+            head = head.next;
+        }
+    }
+    
+    public int getRandom() {
+        int i = (int)(Math.random() * this.nodes.size());
+        return this.nodes.get(i);
+    }
+}
