e8b99c99891542d8befc498d66ccf051e5377ef6
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-03-14 15:25:29 +0530

    LetterCombinations solution 2

---
+++ LeetCode/src/LetterCombinations/LetterCombinations2.java
@@ -0,0 +1,39 @@
+public class LetterCombinations2 {
+    class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+        if(digits.length() == 0) return list;
+
+        HashMap<Character, String> hm = new HashMap<>();
+        hm.put('2',"abc");
+        hm.put('3',"def");
+        hm.put('4',"ghi");
+        hm.put('5',"jkl");
+        hm.put('6',"mno");
+        hm.put('7',"pqrs");
+        hm.put('8',"tuv");
+        hm.put('9',"wxyz");
+
+        backtrack(digits, 0, hm, new StringBuilder(), list);
+        return list;
+    }
+    private static void backtrack(String digits, int i,HashMap<Character, String> hm, StringBuilder sb, List<String> ans) {
+        if(i == digits.length()) {
+            ans.add(sb.toString());
+            return;
+        }
+        String curr = hm.get(digits.charAt(i));
+        for(int k = 0; k < curr.length(); k++) {
+            sb.append(curr.charAt(k));
+            backtrack(digits, i+1, hm, sb, ans);
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String digits = "223";
+        List<String> list = new ArrayList<>();
+        list = letterCombinations(digits);
+        System.out.println(list.toString);
+    }
+}
