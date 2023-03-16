7daebb34c505d448edf6fd654df7e5e30a8f9eba
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-03-16 23:46:34 +0500

    code updated with new solution

--- LeetCode/src/LengthOfLongestSubstring/LongestSubstring2.java
+++ LeetCode/src/LengthOfLongestSubstring/LongestSubstring2.java
@@ -1,25 +1,40 @@
 public class LongestSubstring2 {
-    public static int lengthOfLongestSubstring(String s) {
-        Set<Character>set=new HashSet<>();
-        int maxLength=0;
-        int index=0;
-        for(int i=0;i<s.length();i++){
-           
-            if(!set.contains(s.charAt(i))){
-                set.add(s.charAt(i));
-                maxLength=Math.max(maxLength,i-index+1);
-                
-            }else{
-                while(s.charAt(index)!=s.charAt(i)){
-                    set.remove(s.charAt(index));
-                    index++;
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        int i = -1;
+        int j = -1;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while(true) {
+            boolean f1 = false;
+            boolean f2 = false;
+            while(i < s.length()-1){
+                f1 = true;
+                i++;
+                char ch = s.charAt(i);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+                if(map.get(ch) == 2) {
+                    break;
+                } else {
+                    int len = i - j;
+                    if(len > ans) {
+                        ans = len;
+                    }
                 }
-                set.remove(s.charAt(index));index++;
-                set.add(s.charAt(i));
             }
-            
+
+            while(j < i) {
+                f2 = true;
+                j++;
+                char ch = s.charAt(j);
+                map.put(ch, map.get(ch)-1);
+
+                if(map.get(ch) == 1) {
+                    break;
+                }
+            }
+            if(f1 == false && f2 == false) break;
         }
-        return maxLength;
+        return ans;
     }
     public static void main(String[] args) {
         String str = "pwwkew";
