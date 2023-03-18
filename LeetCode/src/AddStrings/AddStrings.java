3b2c02b49b8c78c01be140f6de301eacd6e2a9d6
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-03-18 20:15:46 +0500

    AddStrings

---
+++ LeetCode/src/AddStrings/AddStrings.java
@@ -0,0 +1,40 @@
+import java.math.BigInteger;
+public class AddStrings {
+    public String addStrings(String num1, String num2) {
+        int i=num1.length()-1;
+        int j=num2.length()-1;
+        int carry=0;
+        ArrayList<Character>ans=new ArrayList<>();
+        while(i>=0 || j>=0){
+            if(i<0){
+                ans.add((char)(((num2.charAt(j)-48+carry)%10)+48));
+                carry=(num2.charAt(j)-48+carry)/10;
+                j--;
+            }
+            else if(j<0){
+                ans.add((char)(((num1.charAt(i)-48+carry)%10)+48));
+                carry=(num1.charAt(i)-48+carry)/10;
+                i--;
+            }
+            else{
+                ans.add((char)((((num1.charAt(i)-48)+(num2.charAt(j)-48)+carry)%10)+48));
+                carry=((num1.charAt(i)-48)+(num2.charAt(j)-48)+carry)/10;
+                i--;
+                j--;
+            }
+        }
+        if(carry!=0){
+            ans.add((char)(carry+48));
+        }
+        String res="";
+        for(int k=ans.size()-1;k>=0;k--){
+            res+=ans.get(k);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String num1 = "11", num2 = "123"
+        System.out.println(addStrings(num1, num2));
+    }
+}
