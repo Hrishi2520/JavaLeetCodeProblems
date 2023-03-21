cc93df575016da60b28b4d09063de914e8486efc
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-03-21 12:45:43 +0500

    updated code

--- LeetCode/src/ZeroFilledSubarrays/ZeroFilledSubarrays.java
+++ LeetCode/src/ZeroFilledSubarrays/ZeroFilledSubarrays.java
@@ -12,6 +12,6 @@ public class ZeroFilledSubarrays {
     }
     public static void main(String[] args) {
       int[] nums = {1,3,0,0,2,0,0,4}
-      System.out.println(zeroFilledSubarray(nums));
+      System.out.println(zeroFilledSubarray(nums.toString()));
   	}
 }
