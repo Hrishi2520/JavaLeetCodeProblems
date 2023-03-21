45380a3d9588209f00e662804768a13056c0730f
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-03-21 12:44:39 +0500

    ZeroFilledSubarrays

---
+++ LeetCode/src/ZeroFilledSubarrays/ZeroFilledSubarrays.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class ZeroFilledSubarrays {
+    public static long zeroFilledSubarray(int[] nums) {
+        long result = 0;
+        for(int i = 0, j = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                j = i + 1;
+            }
+            result += i - j + 1; 
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+      int[] nums = {1,3,0,0,2,0,0,4}
+      System.out.println(zeroFilledSubarray(nums));
+  	}
+}
