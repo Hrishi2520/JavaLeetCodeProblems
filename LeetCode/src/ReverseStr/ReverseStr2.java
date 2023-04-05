ab65f3b25052fda5545be5f8c3b084c7ac96aee1
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-04-05 16:23:31 +0530

    ReverseStr solution 2

---
+++ LeetCode/src/ReverseStr/ReverseStr2.java
@@ -0,0 +1,18 @@
+class ReverseStr2 {
+    public String reverseStr(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        while (s.length() >= 2 * k) {
+            String temp = s.substring(0, k);
+            sb.append(new StringBuilder(temp).reverse());
+            sb.append(s.substring(k, 2 * k));
+            s = s.substring(2 * k);
+        }
+        if (s.length() <= k) {
+            sb.append(new StringBuilder(s).reverse());
+        } else {
+            sb.append(new StringBuilder(s.substring(0, k)).reverse());
+            sb.append(s.substring(k));
+        }
+        return sb.toString();
+    }
+}
