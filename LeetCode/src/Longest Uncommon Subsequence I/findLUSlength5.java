a3808dbceb68afc904e5702af862042e1db6c172
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-05-02 23:44:55 +0530

    Longest Uncommon Subsequence I sol 5

---
+++ LeetCode/src/Longest Uncommon Subsequence I/findLUSlength5.java
@@ -0,0 +1,8 @@
+class findLUSlength5 {
+    public int findLUSlength(String a, String b) {
+        if(a.equals(b)){
+            return -1;
+        }
+        return Math.max(a.length(),b.length());
+    }
+}
