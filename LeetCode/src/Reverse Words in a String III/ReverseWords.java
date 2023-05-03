97c439a28310d00b891508a95077de6c79539863
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-05-03 21:54:13 +0530

    Reverse Words in a String III

---
+++ LeetCode/src/Reverse Words in a String III/ReverseWords.java
@@ -0,0 +1,14 @@
+class ReverseWords {
+    public String reverseWords(String s) {
+        String[] words= s.split(" ");
+        
+        StringBuilder res=new StringBuilder();
+        for(int i=0;i<words.length;i++){
+            StringBuilder temp= new StringBuilder(words[i]);
+            
+            res.append(temp.reverse());
+            res.append(" ");
+        }
+        return res.toString().trim();
+    }
+}
