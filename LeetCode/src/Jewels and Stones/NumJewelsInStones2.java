bce64648351f2db4471ae9ac6ecef1ae2f55338f
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-05-09 15:08:15 +0530

    Jewels and Stones sol 2

---
+++ LeetCode/src/Jewels and Stones/NumJewelsInStones2.java
@@ -0,0 +1,10 @@
+class NumJewelsInStones2 {
+    public int numJewelsInStones(String jewels, String stones) {
+        int n=stones.length();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(jewels.indexOf(stones.charAt(i))>-1) count++;
+        }
+        return count;
+    }
+}
