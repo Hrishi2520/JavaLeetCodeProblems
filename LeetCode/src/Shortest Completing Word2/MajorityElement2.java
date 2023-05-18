d370a4349f47659688d941fec6b247a478437ee1
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-05-18 18:43:37 +0530

    Majority Element sol 2

---
+++ LeetCode/src/Shortest Completing Word2/MajorityElement2.java
@@ -0,0 +1,7 @@
+class MajorityElement2 {
+    public int majorityElement(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
